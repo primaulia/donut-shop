bb6916984e608adf6d3cb8160e6dfdfa30a101f4
Prima Aulia Gusta <primaulia@gmail.com>
2016-10-18 16:33:24 +0800

    added deployment details for heroku

--- public/js/script.js
+++ public/js/script.js
@@ -3,26 +3,17 @@ $(document).ready(function ($) {
 
   $userForm.on('submit', function (e) {
     e.preventDefault()
-    // console.log($(this).serializeArray())
     var formdata = $(this).serializeArray()
 
-    // console.log(data[0].name)
-
-    var user_name = $('#user-name').val()
-    var user_password = $('#user-password').val()
-    var user_email = $('#user-email').val()
-
-    console.log(user_name, user_password, user_email)
-    alert('ajax call now')
-    $.ajax({
-      type: 'POST',
+    window.alert('ajax call now')
+    $.post({
       url: '/api/users',
       data: formdata
     }).done(doSomething)
   })
 
   function doSomething (data) {
-    alert('form submitted, new users created')
+    window.alert('form submitted, new users created')
     $('#all-user-list').append('<li>' + data.name + data.email + data.password + '</li>')
   }
 })
